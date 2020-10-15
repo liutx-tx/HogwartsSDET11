f2319bdcc15dbf1bb8b5430165bd7bcbaebf05c1
ltx <liutx@ltxdeMacBook-Pro.local>
2020-10-15 13:51:31 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+print(2 +2)
+print(r'C:\some\name')
+print('py' 'thon')
+x='abc'
+print(f'C:\\some\\name\\' + x)
+print('C:\\some\\name\\{y}\\{x}'.format(y=x,x=123))
+
+squares = [1,4,9,16,25]
+squares
+
