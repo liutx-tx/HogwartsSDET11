0d8af85679aac241f14e268f3e716be747ec624a
liutx <1213069447@qq.com>
2020-10-21 20:58:31 +0800

    ceshi

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+from hogwarts.sdet.Student import Student
+
 print(2 +2)
 print(r'C:\some\name')
 print('py' 'thon')
@@ -6,5 +8,16 @@ print(f'C:\\some\\name\\' + x)
 print('C:\\some\\name\\{y}\\{x}'.format(y=x,x=123))
 
 squares = [1,4,9,16,25]
-squares
+aaaaaa=10
+print(aaaaaa)
+
+def x(a,b=1,*c,**d):
+    print(f"a={a}")
+    print(f"b={b}")
+    print(f"c={c}")
+    print(f"d={d}")
+
+
+Student()
+
 
